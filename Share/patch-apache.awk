711ab95f6ca918a8af6673c3cabe48162fe47905
Jason Bacon <bacon4000@gmail.com>
2014-10-17 20:45:16 +0000

    Replace patch files with awk script, update index.php
    git-svn-id: file:///home/bacon/SVN/spcm/cluster-admin-0.3@59 fc83dafb-f549-e211-8a70-0017315c2eee

---
+++ Share/patch-apache.awk
@@ -0,0 +1,25 @@
+{
+    if ( $0 ~ "DirectoryIndex index.html" )
+    {
+	print "    DirectoryIndex index.php";
+	print $0;
+    }
+    else if ( $0 ~ "ScriptAlias /cgi-bin/" )
+    {
+	print $0;
+	print "    Alias /ganglia/ \"/usr/local/www/ganglia/\"";
+    }
+    else if ( $0 ~ "<IfModule mime_module>" )
+    {
+	print "<Directory \"/usr/local/www/ganglia\">";
+	print "    Options Indexes FollowSymlinks MultiViews";
+	print "    AllowOverride None";
+	print "    Require all granted";
+	print "</Directory>";
+	print "";
+	print $0;
+    }
+    else
+	print $0;
+}
+
--- WWW/index.php
+++ WWW/index.php
@@ -1,7 +1,7 @@
 <html xml:lang="en" lang="en">
 <head> 
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title>%%SHORT_HOSTNAME%% HPC Cluster</title>
+    <title>%%HOSTNAME%% HPC Cluster</title>
     <link type="text/css" rel="stylesheet" href="global_styles.css">
 </head>
 
@@ -9,41 +9,21 @@
 
 <hr>
 
-<h1>Welcome to the %%SHORT_HOSTNAME%% HPC Cluster</h1>
+<h1>Welcome to the %%HOSTNAME%% HPC Cluster</h1>
 
 <hr>
 
-<h2>Current %%SHORT_HOSTNAME%% Status</h2>
+<h2>Current %%HOSTNAME%% Status</h2>
 
 <p>
-<a href="ganglia/?c=%%SHORT_HOSTNAME%%">Ganglia Resource Monitor</a>
+<a href="ganglia/">Ganglia Resource Monitor</a>
 <br>
-
-<h2>Local Software Documentation</h2>
-
-<p>
-This is a link to the software documentation bundled with each of the
-software packages installed on %%SHORT_HOSTNAME%%.  Each software project chooses
-their own documentation format, so this documentation is provided as-is.
-</p>
-
-<p>
-For more information on any of these software packages, visit the software
-project's website.
-</p>
-
-<p>
-Postscript documentation (.ps extension) can be opened by most PDF viewers.
-</p>
-
-<p>
-<a href="Head-node-docs">Documentation for Software on the Head Node</a>
 
 <h2>FreeBSD Ports Collection</h2>
 
 <p>
-Users can request that additional software be installed on %%SHORT_HOSTNAME%%'s
-head node or compute nodes.  %%SHORT_HOSTNAME%% runs a Unix variant called FreeBSD,
+Users can request that additional software be installed on %%HOSTNAME%%'s
+head node or compute nodes.  %%HOSTNAME%% runs a Unix variant called FreeBSD,
 which has one of the most extensive pre-packaged software collections of
 any operating system, known as the "FreeBSD ports" collection.  Users can
 browse the ports collection by category to see what's available:
@@ -52,8 +32,8 @@ browse the ports collection by category to see what's available:
 <a href="http://www.freebsd.org/ports/categories-grouped.html">http://www.freebsd.org/ports/categories-grouped.html</a>
 
 <p>
-Software not supported by FreeBSD ports can also be requested, but may
-take longer to install.
+Software not currently included in the FreeBSD ports collection can also
+be requested, but may take longer to install.
 
 <p>
 <img style="float:right" src="powerlogo.gif" alt="[Powered by FreeBSD]">
